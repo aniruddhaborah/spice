08f4e6d9ba3d067a42733d5a79fdd97d1bf02cb2
Aniruddha Borah <aniruddha@swiftmedical.io>
2019-05-06 17:03:06 -0400

    Initial Commit

---
+++ Spice/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Spice
+//
+//  Created by Aniruddha Borah on 2019-05-06.
+//  Copyright © 2019 Impy. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Spice/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Spice
+//
+//  Created by Aniruddha Borah on 2019-05-06.
+//  Copyright © 2019 Impy. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
